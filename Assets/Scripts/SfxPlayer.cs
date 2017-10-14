c0b687f7ffe3ee11a3938d8ad5098be850a2adbc
dmj0shua1 <dmj0shua1@gmail.com>
2017-10-14 14:16:48 +0800

    sfxplayer fixed

--- Assets/Scripts/SfxPlayer.cs
+++ Assets/Scripts/SfxPlayer.cs
@@ -1,24 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SfxPlayer : MonoBehaviour {
+public class SfxPlayer : MonoBehaviour
+{
     public GameObject objSoundSource;
     private AudioSource asSfx;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void playSfx()
     {
-if (PlayerPrefs.GetInt("Sfx") == 1) { 
-        asSfx = objSoundSource.GetComponent<AudioSource>();
-         asSfx.Play();
+        if (PlayerPrefs.GetInt("Sfx") == 1)
+        {
+            asSfx = objSoundSource.GetComponent<AudioSource>();
+            asSfx.Play();
         }
     }
-
+}
