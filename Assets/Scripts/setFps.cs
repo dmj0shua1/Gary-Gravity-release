26c89a25f46845c9f16a688dde5ee17cb5f701c0
dmj0shua1 <dmj0shua1@gmail.com>
2017-11-11 09:27:18 +0800

    update IAP

---
+++ Assets/Scripts/setFps.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setFps : MonoBehaviour {
+    [SerializeField]
+    private int Fps = 30;
+    [SerializeField]
+    private int vSync = 0;
+	// Use this for initialization
+	void Start () {
+        QualitySettings.vSyncCount = vSync;
+        Application.targetFrameRate = Fps;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
